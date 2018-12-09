ced415105b2ba0b30fe676e26af7c7075e928fc8
solinguo <552002499@qq.com>
2018-12-09 12:17:20 +0800

    fixed flash write err

--- bootloader/dfu.c
+++ bootloader/dfu.c
@@ -393,6 +393,9 @@ bool dfuUploadStarted() {
 }
 
 void dfuFinishUpload() {
+
+	// Full erase flash
+	flashErasePages(USER_CODE_FLASH, 100);
     while (1) {
         if (userFlash) {
             if (code_copy_lock == BEGINNING) {
